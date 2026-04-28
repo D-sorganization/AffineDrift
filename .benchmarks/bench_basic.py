2b16099efc3dcbd570895b980fc9a8f81069f89c
dieterolson <dieterolson@gmail.com>
2026-04-27 18:38:04 -0700

    chore: add docstrings to benchmark run functions

--- .benchmarks/bench_basic.py
+++ .benchmarks/bench_basic.py
@@ -12,6 +12,7 @@ def test_benchmark_drift(benchmark):
     x = np.array([np.pi / 2, np.pi / 4, 1.0, -1.0])
 
     def run():
+        """Execute drift simulation."""
         return double_pendulum_drift(0.0, x)
 
     benchmark(run)
@@ -21,6 +22,7 @@ def test_benchmark_mass_matrix(benchmark):
     """Benchmark the double pendulum mass matrix calculation."""
 
     def run():
+        """Calculate mass matrix."""
         return double_pendulum_mass_matrix(np.pi / 2, np.pi / 4)
 
     benchmark(run)
@@ -31,6 +33,7 @@ def test_benchmark_trajectory_generation(benchmark):
 
     # We use a very short time span to keep the benchmark fast
     def run():
+        """Generate short trajectory."""
         return generate_reference_trajectory((0.0, 0.1), dt=0.01)
 
     benchmark(run)
